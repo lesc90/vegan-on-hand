6dc9af0f1ebc29fdf57dfb1e07be36a923d95e44
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-10-27 15:03:29 -0500

    Add basic ingredient toggle functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Button, Container } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import Ingredients from './Ingredients.jsx';
 
+
 function App() {
+
   return (
     <React.Fragment>
       <Container>
-        <h2>Welcome to Vegan On Hand</h2>
+        <h2 className="welcome">Welcome to Vegan On Hand</h2>
         <Ingredients />
-        <Button color="primary">See Recipes</Button>
+
       </Container>
     </React.Fragment>
   );
--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Grid, Button, Paper } from '@material-ui/core';
+import axios from 'axios';
+import {EDAMAM_API_KEY, APP_ID } from '../config/edamam.js';
+let url = `https://api.edamam.com/search?q=tofu&app_id=${APP_ID}&app_key=${EDAMAM_API_KEY}&health=vegan`;
 
 const Ingredients = () => {
+
+  const [selectedIngredients, setIngredients] = useState({});
+  const [ingredientsList] = useState(['Beans', 'Tofu', 'Tomatoes', 'Potatoes', 'Sweet Potatoes', 'Rice', 'Pasta', 'Tortillas', 'Bread', 'Bell Pepper', 'Cabbage', 'Broccoli', 'Cauliflower', 'Lettuce', 'Avocado', 'Chickpeas', 'Hummus', 'Squash', 'Carrots', 'Cucumbers', 'Nuts', 'Quinoa']);
+  const [recipes, setRecipes] = useState({});
+
+  let handleClick = () => {
+    // map over selected ingredients and built URL with queries
+    // get data from API
+    // axios.get(url)
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(err => {
+    //     console.error(err)
+    //   })
+    console.log('clicked!');
+  }
+
+  let toggleSelect = (e) => {
+    e.target.classList.toggle('selected');
+    if (!selectedIngredients[e.target.innerText]) {
+      setIngredients(selectedIngredients, selectedIngredients[e.target.innerText] = true)
+    } else {
+      setIngredients(selectedIngredients, delete selectedIngredients[e.target.innerText])
+    }
+  }
+
   return (
     <React.Fragment>
       <h4>Start by selecting the ingredients you have on hand</h4>
-      <p>Ingredients go here</p>
+      <Grid container spacing={3}>
+        {ingredientsList.map(ingredient => {
+          return (
+            <Grid item xs={2}>
+              <Paper className='ingredient' onClick={toggleSelect}>{ingredient}</Paper>
+            </Grid>
+          )
+        })}
+      </Grid>
+      <Button color="primary" onClick={(e) => toggleSelect(e)}>See Recipes</Button>
     </React.Fragment>
   )
 }
